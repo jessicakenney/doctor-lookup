9dff606dbe893e7d9ce05f59104fc156cc738758
jessicakenney <jessicakenney@yahoo.com>
2017-09-16 00:26:33 -0700

    specialty definitions in tooltip

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -4,42 +4,72 @@ import { Ref } from './../js/doctor-lookup.js';
 
 $(document).ready(function() {
 
+
+ //--------------- Display Functions --------------------//
   let displaySpecialties = function(categories){
     $("#show-categories").show();
     categories.forEach(function(category){
-      category.specialties.sort();
-      console.log("CATEGORY : "+category.name+" SPECIALTIES :  "+category.specialties);
+      //category.specialties.name.sort();
+      //console.log("CATEGORY : "+category.name+" SPECIALTIES :  "+category.specialties.name);
 
       // Category List
       $('ul#category-list').append(`<li class="specialty-categories" id="${category.name}"> ${category.name}</li>`);
 
-      // Specialty Lists by Category
-      $('ul#specialty-list').prepend(`<h3 class="show-title" id="show-${category.name}-title">[${category.name}]</h3>`);
+      // Specialty Lists (by Category)
+      $('ul#specialty-list').prepend(`<h3 class="show-title-cat" id="show-${category.name}-title">[${category.name}]</h3>`);
+
       category.specialties.forEach(function(specialty){
-        $('ul#specialty-list').append(`<li class="show-${category.name}-specialties" id="${specialty}">${specialty}</li>`);
+        $('ul#specialty-list').append(`<li class="show-${category.name}-specialties" id="${specialty.uid}"data-toggle="tooltip" data-placement="bottom" title="${specialty.description}">${specialty.name}</li>`);
       });
+    });
+ };
+
+ let displayDoctors = function(doctors,specialty){
+    $('ul#doctor-list').prepend(`<h3 class="show-title-doc" id="show-${specialty}-title">[${specialty}]</h3>`);
+    doctors.forEach(function(doctor){
+      //console.log("DOCTOR"+doctor.first+" "+doctor.last+" Specialty"+specialty);
+      $('ul#doctor-list').append(`<li class="${doctors-specialty}" id="${doctor.last}"> ${doctor.first} ${doctor.last} ${doctor.title}</li>`);
+
+      // //-----------Modal for remaining doctorinfor?
+      // <!-- Button trigger modal -->
+      // <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+      //   Launch demo modal
+      // </button>
+      //
+      // <!-- Modal -->
+      // <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      //   <div class="modal-dialog" role="document">
+      //     <div class="modal-content">
+      //       <div class="modal-header">
+      //         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      //         <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      //       </div>
+      //       <div class="modal-body">
+      //         ...
+      //       </div>
+      //       <div class="modal-footer">
+      //         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      //         <button type="button" class="btn btn-primary">Save changes</button>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
+      // //-----------End Modal for remaining doctorinfor?
+
 
     });
  };
 
- //--------------- Specialty - Option --------------------//
+ //--------------- Find By Specialty Option --------------------//
  $('#specialty-option').submit(function(event) {
     event.preventDefault();
     //alert("You are in specialtyoption");
     $("#category-list li").remove();
     let doctorLookup = new DoctorLookup();
 
-    //doctorLookup.getSpecialties(displaySpecialties);
-
-    // API call to get data
-    // doctorLookup.getSpecialties(displaySpecialties).then(function(data){
-    //     showSpecialties(data);
-    // }).catch(function(err){
-    //     console.log("Oops, something went wrong", err);
-    // });
-
     doctorLookup.getSpecialties(displaySpecialties);
 
+ //--------------- Category- click --------------------//
   $("ul#category-list").on('click', 'li',function(){
     let category = $(this).attr("id");
     // let re=/\d+/;
@@ -53,23 +83,41 @@ $(document).ready(function() {
      //hide everything ..then show what was clicked
      $("#show-specialties").show();
      $("#specialty-list li").hide();
-     $(`.show-title`).hide();
+     $(`.show-title-cat`).hide();
      $(`.show-${category}-specialties`).show();
      $(`#show-${category}-title`).show();
+
+     //---Initialize
+     $(function () {
+       $('[data-toggle="tooltip"]').tooltip()
+     })
   });
 
+ //--------------- Specialty- click --------------------//
   $("ul#specialty-list").on('click', 'li',function(){
     let specialty = $(this).attr("id");
         console.log("THIS  "+$(this));
         console.log("ID   "+specialty);
       //when we click on a speciality, do a API request for dr's
       //in that specialty
-    //doctorLookup.getDoctorBySpecialty(specialty,displayDoctors);
+    doctorLookup.getDoctorsBySpecialty(specialty,displayDoctors);
+     $("#show-doctors").show();
+
+     $("#doctor-list li").hide();
+     $(`.show-title-doc`).hide();
+     $(`.show-${specialty}-doctors`).show();
+     $(`#show-${specialty}-title`).show();
 
   });
+ //--------------- Doctor Name Click- click --------------------//
 
+  $("ul#doctor-list").on('click', 'li',function(){
+     let doctorId = $(this).attr("id");
+         console.log("THIS  "+$(this));
+         console.log("ID   "+doctorId);
 
+         //trigger modal by id
   });
 
-
+  });
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,6 +1,26 @@
 var apiKey = require('./../.env').apiKey;
 
-export class Ref {
+export class DoctorRef{
+  constructor(first,last,title,accepts,street,city,zip,number){
+    this.first = first;
+    this.last = last;
+    this.title = title;
+    this.accepts = accepts;
+    this.street = street;
+    this.city = city;
+    this.zip = zip;
+    this.number = number;
+  }
+}
+
+export class SpecialtyRef{
+  constructor(name, uid, description){
+    this.name = name;
+    this.uid = uid;
+    this.description = description;
+  }
+}
+export class CategoryRef {
   constructor(name,specialties){
     this.name = name;
     this.specialties = specialties;
@@ -12,40 +32,68 @@ export class DoctorLookup{
   constructor(){
   }
 
-
   getSpecialtiesByCategory(specialties){
     let categoryArray = [];
     let categories = [];
     let init = 0;
 
     specialties.data.forEach(function(specialty){
-
-    let found = 0;
-    if (categories.includes(specialty.category)){
-        console.log("****Category Exists  "+specialty.category);
-        //need to find where in the array
-        for (let i = 0; i < categoryArray.length; i++) {
-          if (categoryArray[i].name === specialty.category) {
-            categoryArray[i].specialties.push(specialty.name);
-            found = 1;
+        let newSpecialty = new SpecialtyRef(specialty.name, specialty.uid, specialty.description);
+        //console.log(`SPECIALTY REF ${newSpecialty.name}, ${newSpecialty.uid},${newSpecialty.description}`);
+        let found = 0;
+        if (categories.includes(specialty.category)){
+            //console.log("****Category Exists  "+specialty.category);
+            //need to find where in the array
+            for (let i = 0; i < categoryArray.length; i++) {
+              if (categoryArray[i].name === specialty.category) {
+                //categoryArray[i].specialties.push(specialty.name);
+                categoryArray[i].specialties.push(newSpecialty);
+                found = 1;
+              }
+            }
+          } else {
+            found = 0;
           }
+        //if not found create a new one
+        if (!found){
+          console.log("New category  "+specialty.category);
+          //create a new CategoryRef and push into category Arr
+          //let newRef = new CategoryRef(`${specialty.category}`,[`${specialty.name}`]);
+          let newRef = new CategoryRef(specialty.category,[newSpecialty]);
+          //and push in specialty
+          categoryArray.push(newRef);
+          let foo = categoryArray.length - 1;
+          categories.push(newRef.name);
         }
-      } else {
-        found = 0;
-      }
-    //if not found create a new one
-    if (!found){
-    console.log("New category  "+specialty.category);
-    //create a new ref and push into category Arr
-      let newRef = new Ref(`${specialty.category}`,[`${specialty.name}`]);
-    //and push in specialty
-      categoryArray.push(newRef);
-      console.log("Push into testarray "+newRef.name);
-      categories.push(newRef.name);
-    }
-
     });
     return categoryArray;
+  }
+
+  getDoctorData(doctors){
+    let newDoctors = [];
+      for (let key in doctors ){
+        console.log("Doctors KEY : "+key);
+      }
+    doctors.data.forEach(function(doctor){
+      console.log("Profile"+doctor.profile);
+      console.log("Practices"+doctor.practices);
+      console.log("First"+doctor.profile.first_name);
+      console.log("Title"+doctor.profile.title);
+      console.log("Street"+doctor.practices[0].visit_address.street);
+      //console.log("Accepts"+doctor.accepts_new_patients);
+      // for (let key in doctor ){
+      //   console.log("KEY : "+key);
+      // }
+      // for (let key in doctor.practices[0] ){
+      //   console.log("KEY.practices : "+key);
+      // }
+      // for (let key in doctor.profile ){
+      //   console.log("KEY.profile : "+key);
+      // }
+        let newDoctor = new DoctorRef(doctor.profile.first_name, doctor.profile.last_name, doctor.profile.title, doctor.practices[0].accepts_new_patients, doctor.practices[0].visit_address.street, doctor.practices[0].visit_address.city, doctor.practices[0].visit_address.zip, doctor.practices[0].phones.number);
+        newDoctors.push(newDoctor);
+      });
+    return newDoctors;
   }
 
   getSpecialties(displaySpecialties) {
@@ -56,8 +104,25 @@ export class DoctorLookup{
         displaySpecialties(this.getSpecialtiesByCategory(data));
       })
       .fail ( () =>{
-        console.log("something went wrong");
+        console.log("getSpecialties: something went wrong");
       });
   }
+  getDoctorsBySpecialty(specialty, displayDoctors) {
+    let data;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=45.55%2C-122.63%2C10&user_location=45.55%2C-122.63&sort=last-name-asc&skip=0&limit=25&user_key=${apiKey}`;
+
+    $.get(url)
+      .then( (data) => {
+        displayDoctors(this.getDoctorData(data),specialty);
+      })
+      .fail ( () =>{
+        console.log("getDoctorsBySpecialty: something went wrong");
+      });
+  }
+
+
+
+
+
 
 }
